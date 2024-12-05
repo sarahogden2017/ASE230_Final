fff41776a1a3a2b845603f6a91f9cb61939d350a
sarahogden2017 <sarahogden2017@gmail.com>
2024-12-05 17:24:06 -0500

    fixed sign in errors

--- login_scripts/sign_in.php
+++ login_scripts/sign_in.php
@@ -1,29 +1,32 @@
 <?php
-    if (strlen($_POST['username_signin']) > 0 && strlen($_POST['password_signin']) > 0) {
-        $username = $_POST['username_signin'];
-        $password = $_POST['password_signin'];
-        // check for user in database
-        $sql = "SELECT * FROM users WHERE username = :username";
-        $stmt = $db->prepare($sql);
-        $stmt->execute(['username' => $username]);
-        if ($stmt->rowCount() == 0) {
-            $message = "User doesn't exists.";
+    session_start();
+    require_once('db.php');
+
+    $username = $_POST['username_signin'];
+    $password = $_POST['password_signin'];
+    // check for user in database
+    $sql = "SELECT * FROM users WHERE username = :username";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(['username' => $username]);
+
+    $stmt->execute(['username' => $username]);
+    if ($stmt->rowCount() == 0) {
+        $message = "User doesn't exists.";
+    }
+    // check if password is correct
+    else {
+        $user = $stmt->fetch();
+        if ($user['password_hash'] == $password) {
+            // Set session variables
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['stories_contributed'] = $user['stories_contributed'];
+            $_SESSION['user_id'] = $user['user_id'];
+            header("Location: ../post/index.php");
+            exit();
         }
-        // check if password is correct
         else {
-            $user = $stmt->fetch();
-            if ($user['password_hash'] == $password) {
-                // Set session variables
-                $_SESSION['username'] = $user['username'];
-                $_SESSION['email'] = $user['email'];
-                $_SESSION['stories_contributed'] = $user['stories_contributed'];
-                $_SESSION['id'] = $user['id'];
-                header("Location: ./post/index.php");
-                exit();
-            }
-            else {
-                $message = "Incorrect password.";
-            }
+            $message = "Incorrect password.";
         }
     }
 ?>
