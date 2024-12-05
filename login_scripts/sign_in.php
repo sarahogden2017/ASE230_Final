7c712abcc1790bbae7d94c867b11561d3e3be278
sarahogden2017 <sarahogden2017@gmail.com>
2024-12-05 17:10:59 -0500

    half way done w signin

--- login_scripts/sign_in.php
+++ login_scripts/sign_in.php
@@ -3,9 +3,23 @@
         $username = $_POST['username_signin'];
         $password = $_POST['password_signin'];
         // check for user in database
-
+        $sql = "SELECT * FROM users WHERE username = :username";
+        $stmt = $db->prepare($sql);
+        $stmt->execute(['username' => $username]);
+        if ($stmt->rowCount() == 0) {
+            $message = "User doesn't exists.";
+        }
         // check if password is correct
-        
+        else {
+            $user = $stmt->fetch();
+            if ($user['password_hash'] == $password) {
+                // Set session variables
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['email'] = $user['email'];
+                $_SESSION['stories_contributed'] = $user['stories_contributed'];
+                $_SESSION['id'] = $user['id'];
+            }
+        }
     }
 ?>
 
