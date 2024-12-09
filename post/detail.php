2ab51d1f938750706674aade572b72a71c87dcaa
sarahogden2017 <sarahogden2017@gmail.com>
2024-12-09 17:09:59 -0500

    detail.php done

--- post/detail.php
+++ post/detail.php
@@ -2,12 +2,49 @@
 	session_start();
 
 	$i=$_GET['post_id'];
+
+	require_once('../login_scripts/db.php');
+	$connection = $db;
 	
-	function get_entity() {
-		$jsonString = file_get_contents('./posts.json');
-		$entity_array = json_decode($jsonString, true);
-		return $entity_array;
+	function get_prompt($prompt_id, $db) {
+		$sql = 'SELECT * FROM users INNER JOIN writing_prompts ON users.user_id = writing_prompts.user_id WHERE prompt_id=:prompt_id;';
+		$stmt = $db->prepare($sql);
+    	$stmt->execute(['prompt_id' => $prompt_id]);
+		$prompt = $stmt->fetch(PDO::FETCH_ASSOC);
+		return $prompt;
 	}
+
+	function display_prompt($prompt) {
+		echo "
+			<div class='d-flex justify-content-center align-items-center'>
+				<h1 class='bg-primary text-white p-3 w-100'>{$prompt['prompt_text']}</h1>
+			</div>
+			<div class='container text-center'>
+				<h3>Prompted by: <small class='text-body-secondary'>{$prompt['username']}</small></h3>
+				<h4><br /><br />The continuous story starts here:<br /></h4>
+			</div>";
+	}
+
+	function get_posts($prompt_id, $db) {
+		$sql = $sql = 'SELECT * FROM users INNER JOIN writing_posts ON users.user_id = writing_prompts.user_id WHERE prompt_id=:prompt_id;';
+		$stmt = $db->prepare($sql);
+		$stmt->execute(['prompt_id' => $prompt_id]);
+		$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $posts;
+	}
+
+	function display_posts($posts) {
+		foreach($posts as $post) {
+			echo "
+				<div class='row'>
+					<div class='column' style='float:left;width: 50%;'>{$post['username']}</div>
+						<div class='column' style='float:right;width: 50%;'>{$post['text']}</div>
+						<p text-align='center'>{$post['text']}</p>
+					</div>
+				</div>";
+		}
+	}
+
 ?>
 
 <html>
@@ -16,28 +53,16 @@
 	</head>
   	<!--This will be the page that displays the one page that the user has clicked on-->
   	
-	<?php $post = get_entity();?>
+	<header><?php $prompt = get_prompt($i, $connection); ?></header>
 
 	<body>
-		<div class="d-flex justify-content-center align-items-center">
-			<h1 class="bg-primary text-white p-3 w-100"><?=$post[$i]['prompt'];?></h1>
-		</div>
-		<div class="container text-center">
-			<h3>Prompted by: <small class="text-body-secondary"><?=$post[$i]['author'];?></small></h3>
-			<h4><br /><br />The continuous story starts here:<br /></h4>
-		</div>
+		<?php display_prompt($prompt)?>
 		
 		<div class="container">
-			<?php foreach ($post[$i]['story'] as $addition){ ?>
-					<div class="row">
-						<div class="column" style="float:left;width: 50%;"><?=$addition['user']?></div>
-						<div class="column" style="float:right;width: 50%;"><?=$addition['date']?></div>
-						<hr>
-					</div>
-					<p text-align="center">
-						<?=$addition['text']?>
-					</p>
-				<?php } ?>
+			<?php 
+				$post = get_posts($i, $connection);
+				display_posts($post);
+			?>
 			</div>
 		</div>
 	</body>
